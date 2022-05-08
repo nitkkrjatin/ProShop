4ae93deb02764b88b2675296395aa571afbd2b45
nitkkrjatin <jk.aggarwal748@gmail.com>
2022-05-08 14:42:24 +0530

    Added data models

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
 
 const app = express()
+
+connectDB()
 
 app.get('/api/products', (req, res) => {
   res.json(products)
@@ -18,5 +22,8 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server Running in ${process.env.NODE_ENV} mode on Port: ${PORT}`)
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on Port: ${PORT}`.yellow
+      .bold
+  )
 )
