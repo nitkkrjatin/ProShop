e99d88853be88905412273f277d129269ff0e848
nitkkrjatin <jk.aggarwal748@gmail.com>
2022-05-18 17:59:04 +0530

    Added delete item from cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,7 +3,7 @@ import { Link, useParams, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
-import { addToCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
 
 const CartScreen = () => {
   const { id } = useParams()
@@ -25,7 +25,8 @@ const CartScreen = () => {
   }, [dispatch, id, qty])
 
   const removeFromCartHandler = (id) => {
-    console.log('remove')
+    // console.log('remove')
+    dispatch(removeFromCart(id))
   }
   const checkoutHandler = () => {
     navigate('/login?redirect=shipping')
