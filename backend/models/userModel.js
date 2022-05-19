bc86acf35f1b694027e020c1b77aaa0717aed961
nitkkrjatin <jk.aggarwal748@gmail.com>
2022-05-19 20:33:20 +0530

    Added login screen and reducers for it

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,7 +28,7 @@ const userSchema = mongoose.Schema(
 )
 
 userSchema.pre('save', async function (next) {
-  if (this.isModified('password')) {
+  if (!this.isModified('password')) {
     next()
   }
 
